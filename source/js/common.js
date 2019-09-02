0507f27c9a09d293beba2ecbbfb548924d729dff
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-09-02 23:50:26 +0300

    Перевел мобильную версию в проценты

--- source/js/common.js
+++ source/js/common.js
@@ -1,10 +1,34 @@
 document.createElement( "picture" );
 
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
+var nav = document.querySelector(".menu");
+var toggleButton = document.querySelector(".menu__toggle");
 
+nav .classList.remove("menu--no-js");
+
+toggleButton.addEventListener('click', function() {
+  nav.classList.toggle("menu--open");
+});
+
+var tariff = document.querySelector(".profile__link--modal");
+var tariffModal = document.querySelector(".tariff-all");
+var tariffClose = document.querySelector(".tariff__close");
+
+tariff.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  tariffModal.classList.add("tariff-all--open");
+  setTimeout(function(){ tariffClose.focus(); }, 500);
+});
+
+tariffClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  tariffModal.classList.remove("tariff-all--open");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (tariffModal.classList.contains("tariff-all--open")) {
+      tariffModal.classList.remove("tariff-all--open");
+    }
+  }
+});
