1b0858e867c4214481d4f20b407fa53b38b59b88
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-09-08 20:55:37 +0300

    Адаптив главной страници

--- source/js/common.js
+++ source/js/common.js
@@ -1,34 +1,53 @@
-document.createElement( "picture" );
-
-var nav = document.querySelector(".menu");
-var toggleButton = document.querySelector(".menu__toggle");
-
-nav .classList.remove("menu--no-js");
-
-toggleButton.addEventListener('click', function() {
-  nav.classList.toggle("menu--open");
-});
+document.createElement("picture");
 
 var tariff = document.querySelector(".profile__link--modal");
 var tariffModal = document.querySelector(".tariff-all");
 var tariffClose = document.querySelector(".tariff__close");
+var nav = document.querySelector(".menu");
+var toggleButton = document.querySelector(".menu__toggle");
 
-tariff.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  tariffModal.classList.add("tariff-all--open");
-  setTimeout(function(){ tariffClose.focus(); }, 500);
-});
+nav.classList.remove("menu--no-js");
 
-tariffClose.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  tariffModal.classList.remove("tariff-all--open");
-});
+if (tariffModal) {
+  toggleButton.addEventListener('click', function () {
+    nav.classList.toggle("menu--open");
+  });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
+  tariff.addEventListener('click', function (evt) {
     evt.preventDefault();
-    if (tariffModal.classList.contains("tariff-all--open")) {
-      tariffModal.classList.remove("tariff-all--open");
+    tariffModal.classList.add("tariff-all--open");
+    setTimeout(function () {
+      tariffClose.focus();
+    }, 500);
+  });
+
+  tariffClose.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    tariffModal.classList.remove("tariff-all--open");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (tariffModal.classList.contains("tariff-all--open")) {
+        tariffModal.classList.remove("tariff-all--open");
+      }
     }
+  });
+}
+
+window.onscroll = function () {
+  myFunction()
+};
+
+var header = document.getElementById("site-header__inner");
+
+var sticky = header.offsetHeight;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("site-header__inner--fixed");
+  } else {
+    header.classList.remove("site-header__inner--fixed");
   }
-});
+}
